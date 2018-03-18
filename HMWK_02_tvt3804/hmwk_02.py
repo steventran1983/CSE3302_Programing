8fe9d6124c9b386476598863640cc52d0be19207
steventran1983 <baothangvn@gmail.com>
2018-03-17 23:12:12 -0500

    update python

--- HMWK_02_tvt3804/hmwk_02.py
+++ HMWK_02_tvt3804/hmwk_02.py
@@ -1,21 +1,37 @@
-# Dalio, Brian A.
-# dalioba
-# 2018-03-09
+# Tran, Thang
+# 1001-233-804
+# 2018-03-10
 #--------------------------------------------------
 import re
 import sys
 
 #--------------------------------------------------
 def processToken( token ) :
-  # Replace the following line with your code to classify
-  # the string in 'token' according to your Regular
-  # Expressions and print the appropriate message.
 
-  print( '>%s< is the proposed token.' % ( token ) )
+    isID = r"^[a-mN-Z_][n-zA-M0-9_]*$"
+    isInt = r"^(0d|0D)[0-9]+$"
+    isHexInt = r"^(0x|0X)[0-9a-fA-F]+$"
+    isFP = r"^[0-9]+[\.][0-9]+$"
+    isEFP = r"^[0-9]+\.$ | ^\.[0-9]+$"
+
+
+
+    if re.match(isInt, token):
+        print('>%s<  matches INT' % token)
+    elif re.match(isHexInt,token):
+        print('>%s<  matches HEXINT' % token)
+    elif re.match(isFP, token):
+        print('>%s<  matches FP' % token)
+    elif re.match(isEFP,token):
+        print('>%s<  matches EFP' % token)
+    elif re.match(isID, token):
+        print('>%s<  matches ID' % token)
+    else:
+        print('>%s<  does not match.' % token)
 
 #--------------------------------------------------
 def main() :
-  fName = sys.argv[ 1 ]
+  fName = "inputdata_02.txt"
   print( 'processing tokens from ' + fName + ' ...' )
 
   with open( fName, 'r' ) as fp :
